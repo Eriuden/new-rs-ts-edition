3d2912c1ef8ba2f96df7f747f98ce83ae9f8ad26
Eriuden <antoinecarrette@hotmail.fr>
2025-06-06 09:34:31 +0200

    build: user actions 1 06/06/2025

---
+++ Client/src/redux/actions/User.action.ts
@@ -0,0 +1,95 @@
+import axios from "axios";
+
+export const GET_USER = "GET_USER"
+export const  UPLOAD_PICTURE = "UPLOAD_PICTURE"
+export const  UPDATE_USER = "UPDATE_USER"
+export const DELETE_USER = "DELETE_USER"
+export const  FOLLOW_USER = "FOLLOW_USER"
+export const  UNFOLLOW_USER = "UNFOLLOW_USER"
+
+export const GET_USER_ERRORS = "GET_USER_ERRORS"
+
+export const getUser = (uid: string, dispatch: any) => {   
+        return axios
+            .get(`${process.env.REACT_APP_API_URL}api/user/${uid}`)
+            .then((res) => {
+                dispatch({type:GET_USER, payload: res.data})
+            })
+            .catch((err) => console.log(err))
+}
+
+export const uploadPicture = (data: any, id: string, dispatch: any) => {   
+        return axios
+            .post(`${process.env.REACT_APP_API_URL}api/user/upload`, data)
+            .then((res) => {
+                if (res.data.errors) {
+                    dispatch({ type: GET_USER_ERRORS, payload: res.data.errors})
+                } else {
+                    dispatch ({ type: GET_USER_ERRORS, payload:""})
+                    return axios
+                    .get(`${process.env.REACT_APP_API_URL}api/user/${id}`)
+                    .then((res) => {
+                        dispatch({ type: UPLOAD_PICTURE, payload:res.data.picture})
+                    })
+                }
+            })
+            .catch((err) => console.log(err))
+}
+
+export const updateUser = (userId: string, pseudo: string, bio: string, 
+    gender: string, address: string, dispatch: any) => {
+    
+        return axios({
+            method:"put",
+            url: `${process.env.REACT_APP_API_URL}api/user` + userId,
+            data: {pseudo, bio, gender, address}
+        })
+            .then(() => {
+                dispatch({type: UPDATE_USER, payload: pseudo, bio, gender})
+            })
+            .catch((err) => window.alert(err))
+    
+}
+
+
+export const deleteUser = (userId, pseudo, email, password) => {
+    return (dispatch) => {
+        return axios({
+            method:"delete",
+            url: `${process.env.REACT_APP_API_URL}api/user/${userId}`,
+            data: {pseudo, email, password},
+        })
+        .then((res) => {
+            dispatch({ type: DELETE_USER, payload: { userId }})
+        })
+        .catch((err) => console.log(err))
+    }
+}
+
+export const followUser = (followerId, idToFollow) => {
+    return(dispatch) => {
+        return axios({
+            method:"patch",
+            url: `${process.env.REACT_APP_API_URL}api/user/follow/` + followerId,
+            data: { idToFollow },
+        })
+            .then((res) => {
+                dispatch({ type: FOLLOW_USER, payload:{idToFollow}})
+            })
+            .catch((err) => console.log(err))
+    }
+}
+
+export const unfollowUser = (followerId, idToUnfollow) => {
+    return(dispatch) => {
+        return axios({
+            method:"patch",
+            url: `${process.env.REACT_APP_API_URL}api/user/unfollow/` + followerId,
+            data: { idToUnfollow },
+        })
+            .then((res) => {
+                dispatch({ type: UNFOLLOW_USER, payload:{idToUnfollow}})
+            })
+            .catch((err) => console.log(err))
+    }
+}
