c5e3932316a0cc5529974f41627a421f66a22cdd
Eriuden <antoinecarrette@hotmail.fr>
2025-06-09 11:12:35 +0200

    build: app & context 09/06/2025

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Header from './Components/Header'
+import { useDispatch } from "react-redux" 
+import { getUser } from './redux/actions/User.action'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  type appDispatch = () => any 
+
+  const [uid, setUid] = useState("")
+  const useAppDispatch = () => useDispatch<appDispatch>()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const fetchToken = async () => {
+    await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials: true
+    })
+    .then((res) => {
+      console.log(res);
+      setUid(res.data)
+    })
+    .catch(() => console.log("Pas de tokens"))
+    }
+    fetchToken()
+
+    if (uid) getUser(uid, dispatch)
+  }, [uid])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Header/>
+      <Searcher/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/user-profile/:id" element={<UserProfile/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/messages/:id" element={<MessageIndex/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
