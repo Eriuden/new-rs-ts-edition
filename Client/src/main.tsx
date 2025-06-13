71be298892fa17e5e02c5499556e06e98a90a0f8
Eriuden <antoinecarrette@hotmail.fr>
2025-06-13 11:45:04 +0200

    build: main 13/06/2025

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom"
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Provider } from 'react-redux';
+import reducers from './redux/reducers/indexReducer';
+import { configureStore} from "@reduxjs/toolkit"
+import { getUsers } from './redux/actions/Users.action';
+import { useDispatch } from 'react-redux';
+
+type appDispatch = () => any
+const useAppDispatch = () => useDispatch<appDispatch>()
+const dispatch = useAppDispatch()
+const store = configureStore({reducer:reducers})
+
+store.dispatch(getUsers(dispatch))
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
