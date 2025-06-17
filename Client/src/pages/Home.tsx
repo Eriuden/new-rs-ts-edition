30d2e98935ed4e33643634c4a55c15b35024f883
Eriuden <antoinecarrette@hotmail.fr>
2025-06-17 09:26:44 +0200

    build: Home 17/06/2025

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,8 +1,42 @@
-
+import {useState, useEffect} from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getUsers } from "../redux/actions/Users.action" 
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [loadCard, setLoadCard] = useState(false)
+    const [count, setCount] = useState(0)
+    const dispatch = useDispatch()
+    const users = useSelector((state:any)=> state.allUsersReducer)
+  
+  
+    const loadMore = () => {
+      if (window.innerHeight + document.documentElement.scrollTop + 1 >
+        document.scrollingElement!.scrollHeight)
+        {
+          setLoadCard(true)
+        }
+    }
+  
+    useEffect(()=> {
+      if (loadCard) {
+        getUsers(dispatch,count)
+        setLoadCard(false)
+        setCount(count + 10)
+      }
+      window.addEventListener("scroll", loadMore)
+    }, [loadCard, dispatch, count])
+    
+    return (
+      <div>
+        <div>
+          <ul>
+            {!isEmpty(users[0]) &&
+              users.map((article:any) => {
+                return <UserCard articleProps={article} key={article._id}/>
+            })}
+          </ul>
+        </div>
+      </div>
+    )
 }
 
--- Client/src/redux/actions/Users.action.ts
+++ Client/src/redux/actions/Users.action.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export const GET_USERS = "GET_USERS"
 
-export const getUsers = (dispatch: any) => {   
+export const getUsers = (dispatch: any,number:number) => {   
         return axios
         .get(`${process.env.REACT_APP_API_URL}api/user`)
         .then((res) => {
-            dispatch({ type: GET_USERS, payload: res.data})
+            dispatch({ type: GET_USERS, payload: res.data[number]})
         })
         .catch((err) => console.log(err))
 }
